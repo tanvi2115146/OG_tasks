9913ef6ca56f93bfb70f6d3e004d958708e6644a
Tanvi <tanvi.33012@venturepact.com>
2025-03-27 09:53:44 +0530

    day3

--- day3/promise.js
+++ day3/promise.js
@@ -1,13 +1,10 @@
 
 let url="https://dummyjson.com/products/search?q=phone";
 async function fetchData(){
-    try{
         let res=await fetch(url);
         let data=await res.json();
         console.log(data);
-    }
-    catch(e){
-        console.log("error:" , e)
-    }
+    
 }
-fetchData();
+fetchData().catch((c)=>console.log(err))
+    
